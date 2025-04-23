ec6cc69baba9dd0621ff1795f1438489d165abea
Raj Ghosh <rajyajdhd123@gmail.com>
2025-04-23 21:38:55 +0530

    format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
-
 android {
     namespace = "com.example.krishaksathiandroid"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.example.krishaksathiandroid"
         minSdk = 24
@@ -16,7 +14,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -37,9 +34,7 @@ android {
         jvmTarget = "11"
     }
 }
-
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
