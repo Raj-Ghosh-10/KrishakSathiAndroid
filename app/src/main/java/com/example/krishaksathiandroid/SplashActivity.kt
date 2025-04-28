126e212ade09ac4706037efb1a12dc2974d19fcd
Raj Ghosh <rajyajdhd123@gmail.com>
2025-04-28 20:11:16 +0530

    splash screen add

--- app/src/main/java/com/example/krishaksathiandroid/SplashActivity.kt
+++ app/src/main/java/com/example/krishaksathiandroid/SplashActivity.kt
@@ -9,25 +9,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.krishaksathiandroid.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivitySplashBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Fade-in animation setup for text
         val fadeIn = AlphaAnimation(0f, 1f).apply {
             duration = 1500 // 1.5 seconds
             fillAfter = true
         }
-
-        // Start the fade-in animation for both TextViews
         binding.appNameTextView.startAnimation(fadeIn)
         binding.sloganTextView.startAnimation(fadeIn)
-
-        // Move to MainActivity after 2.5 seconds
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
